34dc89652062e8399022acd6b3f9c8b45e499425
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-04 16:11:53 +0200

    Refactor greetUser function for improved structure and readability

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,20 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+greetUser = (name) => {
+    const currentDate = new Date();
+    if (currentDate.getHours() < 13) {
+        console.log("Buongiorno", name);
+    } else if (currentDate.getHours() > 13 && currentDate.getHours() < 17) {
+        console.log("Buon pomeriggio", name);
+    } else {
+        console.log("Buonasera", name);
+    }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+greetUser(name);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
