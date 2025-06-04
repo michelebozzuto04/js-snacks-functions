e94fe01453a1a6fcd06842e010754105b126dc0f
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-04 15:41:58 +0200

    Refactor filterInitials function for improved readability and structure

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filterInitials(array, letter) {
+    let filteredArray = [];
+    for (let i = 0; i < array.length; i++) {
+        currentElement = array[i];
+        if (currentElement[0].toLowerCase() === letter.toLowerCase()) {
+            filteredArray.push(currentElement);
+        }
+    }
+
+    return filteredArray;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const userLetter = String(prompt("Inserisci una lettera:"));
 
+const filteredArray = filterInitials(names, userLetter);
+
+console.log(filteredArray);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
